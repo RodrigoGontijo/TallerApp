b352888dcef2a990d962f32395f789ba0a0cacb2
Rodrigo <rodrigo.gontijo@hotmail.com>
2021-05-16 22:43:20 -0300

    Add databinding and xmls

--- app/src/main/java/com/example/tallerapp/view/MainActivity.kt
+++ app/src/main/java/com/example/tallerapp/view/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.tallerapp.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.tallerapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
